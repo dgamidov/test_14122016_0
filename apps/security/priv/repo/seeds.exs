77e3e0e3004b21dd7c7973d91b952a3e725013ca
Denis Gamidov <dgamidov@gmail.com>
2016-12-14 19:10:14 +0300

    Add User seed data

--- apps/security/priv/repo/seeds.exs
+++ apps/security/priv/repo/seeds.exs
@@ -12,6 +12,7 @@
 
 alias Security.Repo
 alias Security.Role
+alias Security.User
 
 #
 # Add default roles
@@ -20,4 +21,22 @@ with roles <- ~w(admin customer manager) do
   for role <- roles do
     Repo.get_by(Role, name: role) || Repo.insert!(%Role{name: role})
   end
+end
+
+#
+# Add default users
+
+users = [
+  {"admin@test.com", "admin"},
+  {"customer@test.com", "customer"},
+  {"manager@test.com", "manager"}
+]
+
+for {username, role_name} <- users do
+   role = Role |> Repo.get_by(name: role_name)
+
+   Repo.get_by(User, username: username) || %User{}
+   |> User.changeset(%{username: username})
+   |> Ecto.Changeset.put_assoc(:role, role)
+   |> Repo.insert
 end
