657bf689ea372a2ad7434bb13382ca593e13556e
Denis Gamidov <dgamidov@gmail.com>
2016-12-14 18:43:32 +0300

    Add Role migration and model with mix phoenix.gen.model Role roles name:string:unique

---
+++ apps/security/web/models/role.ex
@@ -0,0 +1,19 @@
+defmodule Security.Role do
+  use Security.Web, :model
+
+  schema "roles" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name])
+    |> validate_required([:name])
+    |> unique_constraint(:name)
+  end
+end
